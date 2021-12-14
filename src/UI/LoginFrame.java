5ad77c95b1bee665560822a451d97564758b430b
sherlock <holmesflying222@gmail.com>
2021-12-14 23:30:12 +0800

    add login frame image and add poi(5.0.0) lib

--- src/UI/LoginFrame.java
+++ src/UI/LoginFrame.java
@@ -4,9 +4,11 @@ import javax.swing.*;
 
 public class LoginFrame extends JFrame {
     public LoginFrame(){
-        this.setSize(400 , 500);
+        super("KYUTES Login");
+        this.setSize(400 , 350);
+        this.setResizable(false);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setTitle("Login");
-        this.setIconImage(new ImageIcon("D:\\k\\nknu_logo.png").getImage());
+        String path = this.getClass().getClassLoader().getResource(".").getPath();
+        this.setIconImage(new ImageIcon(path + "/Image/nknu_logo.png").getImage());
     }
 }
